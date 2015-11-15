583e14790cd6498eeacbc8dc8f21aa03cfff0819
Debashis <Debashis@Desktop>
2015-11-15 18:40:54 +0530

    fghfh

--- testPrint/src/testServ/Servlet.java
+++ testPrint/src/testServ/Servlet.java
@@ -15,19 +15,7 @@ public class Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		/*resp.setContentType("application/json");
-		List<Person> list=new ArrayList<>();
-		list.add(new Person("debashis", "haldia"));
-		list.add(new Person("sumit", "kolkata"));
-		list.add(new Person("amit", "tamluk"));
-		list.add(new Person("ramesh", "parkstreet"));
-		list.add(new Person("subahs", "newtown"));
-		list.add(new Person("animesh", "dumdum"));
-		
-		Gson gson=new Gson();
-		PrintWriter pw=resp.getWriter();
-		String json=gson.toJson(list);
-		pw.println(json);*/
+	
 		PrintWriter pw=resp.getWriter();
 		String str[] =req.getParameterValues("name");
 		for(String str1:str){
