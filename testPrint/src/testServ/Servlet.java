393f25b80353517442bc0f4320cb46c394f84ae9
Debashis <Debashis@Laptop>
2015-11-15 13:01:22 +0530

    cccccc

--- testPrint/src/testServ/Servlet.java
+++ testPrint/src/testServ/Servlet.java
@@ -43,6 +43,7 @@ public class Servlet extends HttpServlet {
 	}
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("");
 		doGet(req, resp);
 	}
 }
